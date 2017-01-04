629e4ebd51e5335b4e26b801c418d7b59822e3fe
Tyler Huffer <Thuffer86@gmail.com>
2017-01-04 12:36:03 -0600

    Added clipboard confirmed message

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
 $(document).ready(function() {
     
    var clip = new Clipboard('.btn');
+  clip.on('success', function(e) {
+	$('.copied')
+		.fadeIn(200)
+		.delay(300)
+		.fadeOut(200);
+	e.clearSelection();
+});
+clip.on('error', function(e) {
+	alert('Safari not supported :/');
+});
     
 });
 
